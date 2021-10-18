eb0b398f0f4e6a496f522b69f88f336faaea55cf
Holmurodov <m.kholmurodov1994@gmail.com>
2021-10-18 21:59:11 +0500

    Create dasturlash asoslari 11 dars.py
    11-dars

---
+++ dasturlash asoslari 11 dars.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+""" 11 dars birnechta shartlarni tekshirish
+Created on Thu Oct 14 16:36:49 2021
+
+@author: Mukhammadamin
+"""
+
+
+#kun = input("Bugun qnaday kun?\n>>")
+#harorat = float(input("Harorat nechi?\n>>"))
+#if (kun.lower() == "yakshanba" or kun.lower()== "shanba") and harorat>30:
+ #   print("Cho\'milgani boramiz")
+#elif (kun.lower() == "yakshanba" or kun.lower()== "shanba") and harorat<30:
+#    print("uyda qoling!")
+
+#narx = 15000
+#choy = True
+#salat = True
+#non = False
+#asarti = True
+#kompot = False
+#if choy:
+ #   print("Mijoz choy sotib oldi")
+  #  narx = narx + 3000
+#if salat:
+ #   print("Mijoz salat sotib oldi")
+ #   narx = narx + 5000
+#if non:
+#    print("Mijoz non sotib oldi")
+#    narx = narx + 2000
+#if asarti:
+#    print("Mijoz asarti sotib oldi")
+#    narx = narx + 12000
+#if kompot:
+ #   print("Mijoz kompot sotib oldi")
+ #   narx = narx + 6000
+  #  
+#print(f"Jami narx {narx} so\'m deb xisoblandi")
+
+#m = ("aerolet", "d-calcin", "clovamed")
+#dori = input("Sizga qanday dori vositasi kerak?\n>>>>")
+#if dori == "aerolet":
+ #    print(f"{dori} yo\'talga qarshi dori vositasi hisoblanadi")
+#elif dori.lower() == "d-calcin":    
+ #    print(f"{dori} kalsiy yetishmovchiligida qabul qilinadigan dori vositasi hisoblanadi")
+#elif dori not in m:
+ #    print(f"Kechirasiz, bizning praysda {dori} haqida ma\'lumot  topilmadi")
+
+#son = float(input("iltimos juft son kiriting\n>>"))
+#if son%2:
+#    print("Bu son juft emas")
+#else:
+#    print("raxmat")
+
+#y = int(input("Yoshingiz nechida?\n>>>>"))
+#if y<4 or y>60:
+#    print("Siz uchun kirish  bepul")
+#elif y <18: print("siz uchun kirish chiptasining narxi 10000 so\'m")
+#elif y>18: print("Siz uchun kirish chiptasining narxi 20000 so\'m")
+
+
+#s1 = int(input('Birinchi sonni kiriting\n-----'))
+#s2 = int(input("Ikkinchi sonni kiriting\n-----"))
+#if s1==s2:
+#    print(f"Ikkala kiritilgan son {s1} va {s2} bir-biriga teng")
+#elif s1 < s2: 
+#    print("Birinchi kiritilgan son {s1} kichik {s2} dan")
+#elif s1 > s2:
+#    print(f"Birinchi kiritilgan son {s1} ikkinchi son {s2} dan katta")
+
+#maxsulotlar = ("olma", "banan", "anjir", "limon", "gilos", "xurmo", "olcha", "shaftoli", 
+#               "cofee", "qand", "shakar")
+#savat = ("sut", "qopqoq", "limon", "gilos", "olma")
+#kerak =input("o\'zingizga kerakli bo'lgan maxsulotlarni kiriting")
+#for kerak in savat:
+  #  if kerak in maxsulotlar:
+ #      print(f"bizning do\'konimizda siz so\'ragan {kerak} bor")
+#else: 
+    #print(f"Bizning do\'konimizda siz so\'ragan {kerak} maxsulot yo\'q ") 
+        
+#m = ("olma", "banan", "anjir", "limon", "gilos", "xurmo", "olcha", "shaftoli", 
+ #              "cofee", "qand", "shakar")   
+
+
+#s = []
+#for n in range(5):
+ #   s.append(input(f"Savatga {n+1}-maxsulotni qo'shing"))
+
+#for j in s:
+    #if j in m:
+     #   print(f"Do\'konimizda {j} bor")
+    #else:
+        #print(f"Do\'konimizda {j} yo\'q")
+        
+        
+#foydalanuvchi = ("admin", "logotip", "asotir", "hp", "samsung")
+#login = input("Iltimos yangi login tanlang\n////")
+#if login.lower() in foydalanuvchi:
+#    print("Bu login band qilingan yangi login tanla galvars!")
+#else:
+#    print("Xush kelibsiz, foydalanuvchi")
+    
+
+#son = int(input("Biror sonni kiriting>>>>>>"))
+#for n in range(2,11):
+ #   if not (son%n):
+ #       print(f"{son} soni {n} soniga qoldiqsiz bo\'linadi")
+#else:
+#    print(f"{son} soni {n} soniga qoldiqsiz bo\'linmaydi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
